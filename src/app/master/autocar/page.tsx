25345b1b7c6d6ba1e61836fd72ccc0fd448e6467
Soudigcar <soudigcar@gmail.com>
2026-08-15 00:54:24 -0300

    feat: exibir gates Master da AUTOCAR

--- src/app/master/autocar/page.tsx
+++ src/app/master/autocar/page.tsx
@@ -1,5 +1,6 @@
 import { MasterAutocarCenter } from '@/components/MasterAutocarCenter';
+import { MasterAutocarAccessControl } from '@/components/MasterAutocarAccessControl';
 
 export default function MasterAutocarPage() {
-  return <MasterAutocarCenter />;
+  return <><MasterAutocarCenter /><MasterAutocarAccessControl /></>;
 }
