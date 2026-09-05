b1cda6a79bb20929f2cbb0d80f782cb4ac1d2636
Soudigcar <soudigcar@gmail.com>
2026-09-05 17:00:15 -0300

    fix(landing): separar aparencia do simulador da posicao

--- src/components/campaigns/CampaignVisualEditorInspector.tsx
+++ src/components/campaigns/CampaignVisualEditorInspector.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
+import { CampaignSimulatorAppearanceInspector } from './CampaignSimulatorAppearanceInspector';
 import { CampaignVisualEditorInspector as LegacyCampaignVisualEditorInspector } from './CampaignVisualEditorInspectorLegacy';
 import { registerLandingDraftCommit } from './CampaignLandingEditorBridge';
 import type { LandingDraftV3 } from './CampaignLandingSectionModel';
@@ -10,5 +11,6 @@ export function CampaignVisualEditorInspector(props: any) {
     props.commit(next);
   }), [props.commit]);
 
+  if (props.layer === 'simulator') return <CampaignSimulatorAppearanceInspector {...props} />;
   return <LegacyCampaignVisualEditorInspector {...props} />;
 }
