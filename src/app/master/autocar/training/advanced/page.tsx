b1d827152fa4208532d98b1c1a884a2a0b58dcdf
Soudigcar <soudigcar@gmail.com>
2026-09-11 01:33:12 -0300

    feat(autocar): preserve advanced commercial training editor

---
+++ src/app/master/autocar/training/advanced/page.tsx
@@ -0,0 +1,5 @@
+import { MasterAutocarCommercialTrainingV3 } from '@/components/MasterAutocarCommercialTrainingV3';
+
+export default function MasterAutocarTrainingAdvancedPage() {
+  return <MasterAutocarCommercialTrainingV3 />;
+}
